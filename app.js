0bc819a0151e9315d6cdbaaf5aebd80caa27862a
alikiskandaryan <alikiskandaryan9408@gmail.com>
2023-11-07 17:23:53 +0400

    Single tour api route

--- app.js
+++ app.js
@@ -30,9 +30,31 @@ app.get('/api/v1/tours' , ((req,res) => {
     });
 }));
 
-app.post('/api/v1/tours' , ((req,res) => {
-//    console.log(req.body);
+app.get('/api/v1/tours/:id' , ((req,res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
 
+    // if(id > tours.length){
+        if(!tour){
+
+        return res.status(404).json({
+            status: 'fail',
+            message: 'invalid ID'
+        })
+    }
+
+
+    res.status(200).json({
+       status:'success',
+       data: {
+        tour
+       } 
+    });
+}));
+
+
+app.post('/api/v1/tours' , ((req,res) => {
     const newId = tours[tours.length-1].id+1;
     const newTour = Object.assign({id:newId},req.body)
 
